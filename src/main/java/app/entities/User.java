65950cf835b3ac91063eabff30042ddf59078014
DavidB <david.bernard@fms-ea.com>
2026-05-19 15:47:08 +0200

    getid and setid in user

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -25,6 +25,15 @@ public class User {
   public User() {}
 
   // Getters and Setters
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
   public String getUsername() {
     return username;
   }
